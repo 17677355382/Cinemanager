d0c65e162ce106c5d67fcebe873acb52397be885
17677355382 <812675632@qq.com>
2019-03-26 15:31:04 +0800

    自定义标题栏（3月26日  15:30 ）

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -2,18 +2,76 @@ package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private LinearLayout layoutMenu;
+    private TextView tvTitle;
+    private SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        /** 去掉标题栏 **/
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+    }
+
+    /** 自定义标题栏 **/
+    private void setTitleMenu() {
+        layoutMenu = findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
+            int visible=layoutMenu.getVisibility()==View.VISIBLE ? View.GONE : View.VISIBLE;
+            layoutMenu.setVisibility(visible);
+        });
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText(R.string.bar_title_menu_orders);
+        search = findViewById(R.id.main_sv_search);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(v -> {
+            System.exit(0); });
+    }
+
+    /** 对标题栏的点击监听 **/
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bar_title_tv_add_cinema:
+                tvTitle.setText(R.string.bar_title_menu_add_cinema);
+                break;
+
+            case R.id.bar_title_tv_view_cinema:
+                tvTitle.setText(R.string.bar_title_menu_cinema);
+                break;
+
+            case R.id.bar_title_tv_add_order:
+                tvTitle.setText(R.string.bar_title_menu_add_orders);
+                break;
+
+            case R.id.bar_title_tv_view_order:
+                tvTitle.setText(R.string.bar_title_menu_orders);
+                break;
+
+            default:
+                break;
+        }
     }
 }
