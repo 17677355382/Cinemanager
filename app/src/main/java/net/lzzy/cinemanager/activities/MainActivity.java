3ae13cdc0369f36b2eefd2b38ef6cd670b44157b
17677355382 <812675632@qq.com>
2019-03-27 09:04:51 +0800

    托管代码重构（3月27日  9：05 ）

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,25 +1,34 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.view.Window;
+import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
+import net.lzzy.cinemanager.fragments.CinemasFragment;
+import net.lzzy.cinemanager.fragments.OrdersFragment;
 
 /**
  * @author Administrator
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    private FragmentManager manager=getSupportFragmentManager();
     private LinearLayout layoutMenu;
     private TextView tvTitle;
     private SearchView search;
+    private SparseArray<Fragment> fragmentArray=new SparseArray<>();
+    private SparseArray<String> titleArray=new SparseArray<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /** 自定义标题栏 **/
     private void setTitleMenu() {
+        titleArray.put(R.id.bar_title_tv_add_cinema,"添加影院");
+        titleArray.put(R.id.bar_title_tv_add_order,"添加订单");
+        titleArray.put(R.id.bar_title_tv_view_cinema,"影院列表");
+        titleArray.put(R.id.bar_title_tv_view_order,"订单列表");
         layoutMenu = findViewById(R.id.bar_title_layout_menu);
         layoutMenu.setVisibility(View.GONE);
         findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
@@ -53,25 +66,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     /** 对标题栏的点击监听 **/
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        layoutMenu.setVerticalGravity(View.GONE);
+        tvTitle.setText(titleArray.get(v.getId()));
+        FragmentTransaction transaction=manager.beginTransaction();
+        Fragment fragment=fragmentArray.get(v.getId());
+        if (fragment==null){
+            fragment=createFragment(v.getId());
+            fragmentArray.put(v.getId(),fragment);
+            transaction.add(R.id.fragment_container,fragment);
+        }
+        for (Fragment f:manager.getFragments()){
+            transaction.hide(f);
+        }
+        transaction.show(fragment).commit();
+
+    }
+
+    private Fragment createFragment(int id) {
+        switch (id) {
             case R.id.bar_title_tv_add_cinema:
-                tvTitle.setText(R.string.bar_title_menu_add_cinema);
-                break;
 
+                break;
             case R.id.bar_title_tv_view_cinema:
-                tvTitle.setText(R.string.bar_title_menu_cinema);
-                break;
-
+                return new CinemasFragment();
             case R.id.bar_title_tv_add_order:
-                tvTitle.setText(R.string.bar_title_menu_add_orders);
-                break;
 
-            case R.id.bar_title_tv_view_order:
-                tvTitle.setText(R.string.bar_title_menu_orders);
                 break;
+            case R.id.bar_title_tv_view_order:
+                return new OrdersFragment();
 
             default:
                 break;
         }
+        return null;
     }
 }
